978d963550c299fc95e3a524b4e000372963f496
vanessabach <vanessabach.r@gmail.com>
2022-09-11 15:31:50 -0300

    changes

--- src/components/Empreendimento.tsx
+++ src/components/Empreendimento.tsx
@@ -6,28 +6,34 @@ import OutsideImgTres from '../assets/outsideimg3.svg';
 
 
 export function Empreendimento(){
+
+  
   return (
     <div className={styles.empreendimento} id="empreendimento">
       <div className={styles.description}>
         
         <div className = {styles.texto}>
-          <h2>O Empreendimento</h2>
-          <h3>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut
-             labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem
-              ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. 
-              Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-              Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit
-               anim id est laborum.</h3>
+          <h2>O EMPRENDIMENTO</h2>
+          <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra viverra nisl, id nullam
+             et morbi. Ornare ut est faucibus nec ut curabitur sagittis. Dui risus amet, sit morbi aliquet.
+              In consectetur hendrerit ut lobortis nunc enim fusce morbi sagittis. Bibendum nulla cursus
+               urna habitant integer in orci, amet. Elementum habitasse sem amet sit mi est. Aenean dui,
+                odio malesuada ullamcorper est sed. Aliquet lacus, platea nibh ultricies turpis diam leo
+                 duis. Amet massa, sit rhoncus leo laoreet habitant. 
+                 Aliquam nullam risus duis curabitur.</h3>
         </div>
         <div className = {styles.outsideImg}>
           <img src={OutsideImgUm} alt="" />
           <img src={OutsideImgDois} alt="" />
           <img src={OutsideImgTres} alt="" />
         </div>
+        <div className={styles.footerTexto}>
+          <h2>(click e arraste)</h2>
+        </div>
 
       </div>
-      <div>
-          <img src={PainelEmpreendimento} alt="" />
+      <div className={styles.painelEmpreendimento}>
+        <img src={PainelEmpreendimento} alt="" />
       </div>
       
     </div>  
